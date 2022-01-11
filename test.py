47de0f5abf2666c4018467b5043c8745a3733b93
MyTeams <97308615+Projects-2002@users.noreply.github.com>
2022-01-11 20:41:03 +0530

    Update and rename StocksADP.py to test.py

--- test.py
+++ test.py
@@ -12,34 +12,35 @@ import docx
 import PyPDF2
 from docx2pdf import convert
 import openpyxl
- 
+
+
 def stock_value(url,name):#function defination
     r = get(url)
     soup = BeautifulSoup(r.text, 'lxml')
- 
- 
+
+
     # get all tables
     tables = soup.find_all('table')
- 
+
     # loop over each table
     for num,table in enumerate(tables):
- 
+
         # create filename
         filename = name+'.csv' #naming the file
- 
+
         # open file for writing
         with open(filename, 'w') as f:
- 
+
             # store rows here
             data = []
- 
+
             # create csv writer object
             csv_writer = writer(f)
- 
+
             # go through each row
             rows = table.find_all('tr')
             for row in rows:
- 
+
                 # write headers if any
                 headers = row.find_all('th')
                 if headers:
@@ -54,18 +55,48 @@ def stock_value(url,name):#function defination
                 except:
                     pass
     return filename
- 
+
+def del_prev_stock_info():
+    y=input("\nFiles related to previous execution will be deleted !\nAre you sure to continue execution in path : "+os.getcwd()+" [ Y / N ] : ")
+    y=y.upper()
+    if y=='Y':
+        pass
+    else:
+        print("Please move and re-run this tool in a new folder ...")
+        exit(0)
+    z=os.listdir()
+    for i in z:
+        if(i=='test.py'):
+            pass
+        elif(i=='msedgedriver.exe'):
+            pass
+        elif(i=='chromedriver.exe'):
+            pass
+        elif(os.path.isdir(i)==True):
+            pass
+        else:
+            os.remove(i)
+
+def dir_ispresent(x):
+    if os.path.isdir(x):
+        shutil.rmtree(x)
+    else:
+        pass
+    os.mkdir(x)
+    
 i = 1
 while i == 1:
-    print("1.most active stocks")#menu
-    print("2.gainer stocks")
-    print("3.loser stocks")
-    ch = int(input("Enter your choice:")) #prompting user to choose
+    del_prev_stock_info()
+    print("\n*** KINDLY USE THIS TOOL IN A SEPARATE FOLDER ***\n\n\tMENU")
+    print("1. Most active stocks")#menu
+    print("2. Gainer stocks")
+    print("3. Loser stocks")
+    ch = int(input("\nEnter your choice : ")) #prompting user to choose
     if ch ==1:
         url = 'https://finance.yahoo.com/most-active'
-        file_name ='active_stocks'
+        file_name ='active_stocks' 
         d = stock_value(url,file_name)
- 
+
     if ch ==2:
         url2 = 'https://finance.yahoo.com/gainers'
         file_name = 'stock_gainers'
@@ -75,7 +106,8 @@ while i == 1:
         file_name = 'stock_losers'
         d = stock_value(url3,file_name) #function call
     i +=1
-   
+    
+    
 csvRows = [] #list to store csv values
 csvFileObj = open(d) #d contains the filename returned by the function value
 readerObj = csv.reader(csvFileObj) #creating a reader object
@@ -85,23 +117,23 @@ for row in readerObj: #traversing through each row
     csvRows.append(row) #appending row values
 csvFileObj.close()
 # print(csvRows)
- 
+
 with open(file_name+'.json','w') as jsonFile: #creating a jsonfile by name as that of file_name.
     json.dump(csvRows,jsonFile,indent=2) #dump function is used to write into the json file
-   
+    
 def get_updates(url,option):#function defination
     browser = webdriver.Chrome()
-    browser.get(url)
-    dropdown = browser.find_element_by_css_selector('#chart-toolbar > div:nth-child(1) > div.M\(0\).D\(ib\).Bdrs\(3px\).C\(\$tertiaryColor\).Fz\(s\).Fw\(500\).O\(n\)\:f\!.Mstart\(15px\).Mstart\(10px\)--tab1024.Mstart\(10px\)--tab768.chartTypeBtn.Pos\(r\) > div')
+    browser.get(url) 
+    dropdown = browser.find_element_by_css_selector('#chart-toolbar > div:nth-child(1) > div.M\(0\).D\(ib\).Bdrs\(3px\).C\(\$tertiaryColor\).Fz\(s\).Fw\(500\).O\(n\)\:f\!.Mstart\(15px\).Mstart\(10px\)--tab1024.Mstart\(10px\)--tab768.chartTypeBtn.Pos\(r\) > div.Pos\(r\).D\(ib\).Cur\(p\) > svg.Mstart\(2px\).Va\(m\)\!.Mt\(-2px\).chartTypeBtn\:h_Fill\(\$linkColor\)\!.chartTypeBtn\:h_Stk\(\$linkColor\)\!.Cur\(p\)')
     dropdown.click() #it clicks the drop down menu
-    ele = browser.find_element_by_css_selector(option) #option contains the value we chose for viewing the data
+    ele = browser.find_element_by_css_selector(option) #option contains the value we chose for viewing the data 
     ele.click()
     time.sleep(5) #making the webdriver wait
     screenshot = browser.save_screenshot(stock+'.png') #this is used to take photos or screenshots of the graph
     elem = browser.find_element_by_css_selector('#chart-toolbar > div:nth-child(1) > div.D\(ib\).Whs\(nw\).H\(17px\) > ul > li:nth-child(2) > button > span') #selecting the 5 day change values
     elem.click()
     time.sleep(5)
-    screenshot = browser.save_screenshot(stock+'5d.png')
+    screenshot = browser.save_screenshot(stock+'5d.png') 
     elem2 = browser.find_element_by_css_selector('#chart-toolbar > div:nth-child(1) > div.D\(ib\).Whs\(nw\).H\(17px\) > ul > li:nth-child(3) > button > span') #selecting 1M changes values
     elem2.click()
     time.sleep(5)
@@ -123,7 +155,7 @@ def get_updates(url,option):#function defination
     doc.add_heading(f'{stock.upper()} details', 0) #adding headings to the document
     doc.add_picture(stock+'.png',width=docx.shared.Cm(17.71), height=docx.shared.Cm(12.7)) #inserting the captured images to word
     doc.add_page_break() #in order to make the image come in next line
-    doc.add_heading(f'{stock.upper()} 5d change', 0) #heading
+    doc.add_heading(f'{stock.upper()} 5d change', 0) #heading 
     doc.add_picture(stock+'5d.png',width=docx.shared.Cm(17.71), height=docx.shared.Cm(12.7))
     doc.add_page_break()
     doc.add_heading(f'{stock.upper()} 1M change', 0)
@@ -139,27 +171,20 @@ def get_updates(url,option):#function defination
     doc.add_picture(stock+'1Y.png',width=docx.shared.Cm(17.71), height=docx.shared.Cm(12.7))
     doc.save(stock+'.docx')# saving the docx file
     return(stock+'.docx')# function returns selected stockfile.docx
-
-def dir_ispresent(x):
-    if os.path.isdir(x):
-        os.rmdir(x)
-        os.mkdir(x)
-    else:
-        os.mkdir(x)
-
+    
 while True:
-    stock = input("Enter the stock name you want to see updates on:") #prompting user to enter a stock value
-    dir_ispresent(stock)
+    stock = input("\nEnter the stock name you want to see updates on : ") #prompting user to enter a stock value
     stock.upper()
+    dir_ispresent(stock.upper())
     url4 = 'https://finance.yahoo.com/chart/'+stock+'#eyJpbnRlcnZhbCI6ImRheSIsInBlcmlvZGljaXR5IjoxLCJ0aW1lVW5pdCI6bnVsbCwiY2FuZGxlV2lkdGgiOjgsImZsaXBwZWQiOmZhbHNlLCJ2b2x1bWVVbmRlcmxheSI6dHJ1ZSwiYWRqIjp0cnVlLCJjcm9zc2hhaXIiOnRydWUsImNoYXJ0VHlwZSI6ImxpbmUiLCJleHRlbmRlZCI6ZmFsc2UsIm1hcmtldFNlc3Npb25zIjp7fSwiYWdncmVnYXRpb25UeXBlIjoib2hsYyIsImNoYXJ0U2NhbGUiOiJsaW5lYXIiLCJwYW5lbHMiOnsiY2hhcnQiOnsicGVyY2VudCI6MSwiZGlzcGxheSI6Ik5WREEiLCJjaGFydE5hbWUiOiJjaGFydCIsImluZGV4IjowLCJ5QXhpcyI6eyJuYW1lIjoiY2hhcnQiLCJwb3NpdGlvbiI6bnVsbH0sInlheGlzTEhTIjpbXSwieWF4aXNSSFMiOlsiY2hhcnQiLCLigIx2b2wgdW5kcuKAjCJdfX0sInNldFNwYW4iOnt9LCJsaW5lV2lkdGgiOjIsInN0cmlwZWRCYWNrZ3JvdW5kIjp0cnVlLCJldmVudHMiOnRydWUsImNvbG9yIjoiIzAwODFmMiIsInN0cmlwZWRCYWNrZ3JvdWQiOnRydWUsImV2ZW50TWFwIjp7ImNvcnBvcmF0ZSI6eyJkaXZzIjp0cnVlLCJzcGxpdHMiOnRydWV9LCJzaWdEZXYiOnt9fSwic3ltYm9scyI6W3sic3ltYm9sIjoiTlZEQSIsInN5bWJvbE9iamVjdCI6eyJzeW1ib2wiOiJOVkRBIiwicXVvdGVUeXBlIjoiRVFVSVRZIiwiZXhjaGFuZ2VUaW1lWm9uZSI6IkFtZXJpY2EvTmV3X1lvcmsifSwicGVyaW9kaWNpdHkiOjEsImludGVydmFsIjoiZGF5IiwidGltZVVuaXQiOm51bGwsInNldFNwYW4iOnt9fV0sInN0dWRpZXMiOnsi4oCMdm9sIHVuZHLigIwiOnsidHlwZSI6InZvbCB1bmRyIiwiaW5wdXRzIjp7ImlkIjoi4oCMdm9sIHVuZHLigIwiLCJkaXNwbGF5Ijoi4oCMdm9sIHVuZHLigIwifSwib3V0cHV0cyI6eyJVcCBWb2x1bWUiOiIjMDBiMDYxIiwiRG93biBWb2x1bWUiOiIjZmYzMzNhIn0sInBhbmVsIjoiY2hhcnQiLCJwYXJhbWV0ZXJzIjp7IndpZHRoRmFjdG9yIjowLjQ1LCJjaGFydE5hbWUiOiJjaGFydCIsInBhbmVsTmFtZSI6ImNoYXJ0In19fX0-'
-    print("select a option")#asking user to enter the type in which they want to see the data
-    print("1.Line")
-    print("2.Area")
-    print("3.Candle")
-    print("4.Hollow candle")
-    print("5.Bar")
-    print("6.colored bar")
-    choice1 = int(input("Enter your choice:"))
+    print("\nSelect an option")#asking user to enter the type in which they want to see the data
+    print("1. Line")
+    print("2. Area")
+    print("3. Candle")
+    print("4. Hollow candle")
+    print("5. Bar")
+    print("6. colored bar\n")
+    choice1 = int(input("Enter your choice : "))
     if choice1 ==1:
         option = '#dropdown-menu > ul > li.Cur\(p\).C\(\$linkColor\)\:h.Bgc\(\$hoverBgColor\)\:h.chartBtn.Bgc\(\$hoverBgColor\) > button'
         docs = get_updates(url4,option) #option contains the css selector of the particular type just like if u select "line" then option contains the css selector of the "line"
@@ -179,7 +204,7 @@ while True:
         option = '#dropdown-menu > ul > li:nth-child(6)'
         docs = get_updates(url4,option)
     else:print(f"{ch} is an invalid option ")
-   
+    
     convert(docs) #converting docs to pdf using docx2pdf module
     try:
         convert('C:\\windows\\system32\\stock\\'+docs,'C:\\windows\\system32\\stock\\'+stock+'.pdf')
@@ -195,17 +220,19 @@ while True:
     sheet = workbook.active
     for row in csv_data:
         sheet.append(row)
+
     workbook.save(file_name+'.xlsx')
- 
-    choice = input("do you want to continue:")
+    choice = input("do you want to continue : ")
     if choice == 'y':
+        for i in os.listdir():
+            d=["test.py","msedgedriver.exe","chromedriver.exe","stock_losers.csv","active_stocks.csv","stock_gainers.csv","stock_losers.json","active_stocks.json","stock_gainers.json"]
+            if i in d :
+                pass
+            else:
+                shutil.move(i,'./'+stock)
         pass
     else:
         break
-# x=[]
-# def name_move():
 
-#     x.append(stock+'.')
-# for i in x:
-#     shutil.move(x[0],'./'+stock)
-print("Thank you see you again!!")
+    
+print("Thank you ..See you again !!")
